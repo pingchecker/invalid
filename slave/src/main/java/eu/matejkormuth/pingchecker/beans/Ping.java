f7133f811c0edc140852da3f8b75ac85c73c2e28
Matej Kormuth <dobrakmato@gmail.com>
2015-05-30 00:08:08 +0200

    initial commit

---
+++ slave/src/main/java/eu/matejkormuth/pingchecker/beans/Ping.java
@@ -0,0 +1,86 @@
+/**
+ * slave - Tool to periodically check services availability
+ * Copyright (c) 2015, Matej Kormuth <http://www.github.com/dobrakmato>
+ * All rights reserved.
+ * <p>
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * <p>
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * <p>
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package eu.matejkormuth.pingchecker.beans;
+
+import com.avaje.ebean.annotation.CreatedTimestamp;
+import com.avaje.ebean.validation.NotNull;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "pings")
+@UniqueConstraint(columnNames = "id")
+public class Ping {
+
+    @Id
+    private long id;
+    @NotNull
+    @ManyToOne
+    private Target target;
+    @NotNull
+    @CreatedTimestamp
+    private Timestamp timestamp;
+    private short ping;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Target getTarget() {
+        return target;
+    }
+
+    public void setTarget(Target target) {
+        this.target = target;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public short getPing() {
+        return ping;
+    }
+
+    public void setPing(short ping) {
+        this.ping = ping;
+    }
+
+    public static final class ErrorCodes {
+        public static final short NOT_REACHABLE = -1;
+        public static final short INVALID_ADDRESS = -2;
+    }
+}
